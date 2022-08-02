75b9989ac75c7f26b149ae358d3d85638d970b7d
subalakshmid <subalskhmikd@gmail.com>
2022-08-03 01:35:00 +0530

    suba

---
+++ src/com/test/Emp.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Emp {
+
+		
+		public void  test1() {
+			System.out.println("test1");
+		}
+		public static void main(String[] args) {
+			
+			Emp e = new Emp();
+			e.test1();
+	}
+}
