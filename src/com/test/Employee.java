2e1f811ab2e18640ca6afd40b708288c805d3c0c
subalakshmid <subalskhmikd@gmail.com>
2022-08-03 01:44:10 +0530

    emp

---
+++ src/com/test/Employee.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Employee {
+	
+	public void  test2() {
+		System.out.println("test1");
+	}
+	public static void main(String[] args) {
+		
+		Employee e = new Employee();
+		e.test2();
+}
+}
